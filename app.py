2c7d0354c519779db59ae19b92df76f411537185
jaysen <jaysen@gmail.com>
2023-11-09 19:45:30 +0200

    api reset counter btn + refactor api endpoints

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ counters = {
     'tab2': 0
 }
 
-
 def handle_page(page_name, template_name):
 
     if 'last_page' in session and session['last_page'] != page_name:
         counters[page_name] += 1
     session['last_page'] = page_name
-    return render_template(template_name, counter=counters[page_name])
-
+    return render_template(template_name, counter=counters[page_name], total=sum(counters.values()))
 
 @app.route('/')
 def home():
@@ -40,18 +38,19 @@ def tab2():
 def get_counters():
     return jsonify(counters)
 
-@app.route('/api/counters/home', methods=['GET'])
-def get_home_counter():
-    return jsonify({'count': counters['home']})
+@app.route('/api/counters/<page>', methods=['GET'])
+def get_counter(page):
+    if page in counters:
+        return jsonify({'count': counters[page]})
+    else:
+        return jsonify({'error': 'Invalid page'}), 400
 
-@app.route('/api/counters/tab1', methods=['GET'])
-def get_tab1_counter():
-    return jsonify({'count': counters['tab1']})
-
-@app.route('/api/counters/tab2', methods=['GET'])
-def get_tab2_counter():
-    return jsonify({'count': counters['tab2']})
-
+@app.route('/api/counters/reset', methods=['POST'])
+def reset_counters():
+    counters['home'] = 0
+    counters['tab1'] = 0
+    counters['tab2'] = 0
+    return jsonify(counters)
 
 
 if __name__ == '__main__':
